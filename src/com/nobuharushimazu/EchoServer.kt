f6df829d068972d9f0543abe2675cc188463a53a
bichanna <nobu.bichanna@gmail.com>
2021-11-19 18:55:04 -0600

    Create Kotlin version of EchoServer

---
+++ src/com/nobuharushimazu/EchoServer.kt
@@ -0,0 +1,13 @@
+package com.nobuharushimazu
+
+import java.net.ServerSocket
+import java.net.Socket
+
+fun main() {
+    ServerSocket(6000).use { serverSocket ->
+        while (true) {
+            val socket: Socket = serverSocket.accept()
+            Echoer(socket).start()
+        }
+    }
+}
---
+++ src/com/nobuharushimazu/Echoer.kt
@@ -0,0 +1,29 @@
+package com.nobuharushimazu
+
+import java.net.Socket
+import java.io.*
+
+class Echoer(private val socket: Socket): Thread() {
+
+    override fun run() {
+        try {
+            val input = BufferedReader(InputStreamReader(socket.getInputStream()))
+            val output = PrintWriter(socket.getOutputStream(), true)
+            while (true) {
+                val echoString: String = input.readLine()
+                println("Received client input: $echoString")
+                if (echoString == "exit") {
+                    println("Client disconnected")
+                    break
+                }
+                output.println(echoString)
+            }
+        } catch (e: IOException) {
+            println("Error occurred: ${e.localizedMessage}")
+        } finally {
+            try {
+                socket.close()
+            } catch (ignored: IOException) {}
+        }
+    }
+}
